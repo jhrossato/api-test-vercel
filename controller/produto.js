b393cdf6c8cb1214efb3d091990529f53d889eb7
Joao H Rossato <johrossato@gmail.com>
2024-05-26 17:00:38 -0300

    post put delete

--- controller/produto.js
+++ controller/produto.js
@@ -1,78 +1,47 @@
-//const odbc = require('odbc');
 const db = require("../database/db");
 
 const get = (async (req, res) =>{
     const q = "SELECT * FROM tb_produtos";
     db.query(q, (err, data) =>{
-        if(err){
-            console.log(err)
-            return res.status(500).json(err.message); 
-        } 
-
+        if(err) return res.status(500).json(err.message); 
         return res.status(200).json(data);
-    })
-
-    // await odbc.connect(connectionString, (error, connection) => {
-    //     connection.query('SELECT * FROM tb_produtos', (error, result) => {
-    //         if(error)
-    //             res.status(500).json(error.message); 
-    //         else
-    //             result.length != 0 ? res.status(200).json(result) : res.status(204).json(result)
-    //         connection.close();
-    //     });
-    // });
+    });
 });
 
 const getById = (async (req, res) =>{
-    // const produtoId = req.params.id;
-    // await odbc.connect(connectionString, (error, connection) => {
-    //     connection.query(`SELECT * FROM tb_produtos WHERE id=${produtoId}`, (error, result) => {
-    //         if(error)
-    //             res.status(500).json(error.message); 
-    //         else
-    //             result.length != 0 ? res.status(200).json(result) : res.status(204).json(result)
-    //         connection.close();
-    //     });
-    // });
+    const produtoId = req.params.id;
+    const q = `SELECT * FROM tb_produtos WHERE id=${produtoId}`;
+    db.query(q, (err, data) =>{
+        if(err) return res.status(500).json(err.message); 
+        return data.length != 0 ? res.status(200).json(data) : res.status(204).json(data);
+    });
 });
 
 const post = (async (req, res) => {
-    // const produto = req.body;
-    // await odbc.connect(connectionString, (error, connection) => {
-    //     connection.query(`INSERT INTO tb_produtos (nome, preco, observacao, imagem) VALUES ('${produto.nome}', ${produto.preco}, '${produto.observacao}', '${produto.imagem}')`, (error, result) => {
-    //         if(error)
-    //             res.status(500).json(error.message); 
-    //         else
-    //             res.status(201).json({result:"ok"});
-    //         connection.close();
-    //     });
-    // });
+    const produto = req.body;
+    const q = `INSERT INTO tb_produtos (nome, preco, observacao, imagem) VALUES ('${produto.nome}', ${produto.preco}, '${produto.observacao}', '${produto.imagem}')`;
+    db.query(q, (err, data) =>{
+        if(err) return res.status(500).json(err.message); 
+        return res.status(201).json({result:"ok"});
+    });
 });
 
 const remove = (async (req, res) => {
-    // const produtoId = req.params.id;
-    // await odbc.connect(connectionString, (error, connection) => {
-    //     connection.query(`DELETE FROM tb_produtos WHERE id = ${produtoId}`, (error, result) => {
-    //         if(error)
-    //             res.status(500).json(error.message); 
-    //         else
-    //             res.status(201).json({result:"ok"});
-    //         connection.close();
-    //     });
-    // });
+    const produtoId = req.params.id;
+    const q = `DELETE FROM tb_produtos WHERE id = ${produtoId}`;
+    db.query(q, (err, data) =>{
+        if(err) return res.status(500).json(err.message); 
+        return res.status(200).json({result:"ok"});
+    });
 })
 
 const put = (async (req, res) => {
-    // const produto = req.body;
-    // await odbc.connect(connectionString, (error, connection) => {
-    //     connection.query(`UPDATE tb_produtos SET nome = '${produto.nome}', preco = ${produto.preco}, observacao = '${produto.observacao}', imagem = '${produto.imagem}' WHERE id = ${produto.id}`, (error, result) => {
-    //         if(error)
-    //             res.status(500).json(error.message); 
-    //         else
-    //             res.status(201).json({result:"ok"});
-    //         connection.close();
-    //     });
-    // });
+    const produto = req.body;
+    const q = `UPDATE tb_produtos SET nome = '${produto.nome}', preco = ${produto.preco}, observacao = '${produto.observacao}', imagem = '${produto.imagem}' WHERE id = ${produto.id}`;
+    db.query(q, (err, data) =>{
+        if(err) return res.status(500).json(err.message); 
+        return res.status(201).json({result:"ok"});
+    });
 });
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const produtoRouter = require('./routes/produto');
-
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,10 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/produtos', produtoRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, "0.0.0.0", function () {
   console.log(`Berlocaria API escutando na porta ${process.env.PORT}!`);
